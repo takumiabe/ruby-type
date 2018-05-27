199cfa66d8f45c8488e7f961b07190948262b92f
Takumi Abe <abe@engraphia.com>
2018-05-27 20:45:53 +0900

    wip concept...

--- lib/ruby/type.rb
+++ lib/ruby/type.rb
@@ -1,7 +1,30 @@
 require "ruby/type/version"
+require 'parser/current'
 
 module Ruby
   module Type
-    # Your code goes here...
+    # Ruby::Type.Of('1 + 1')
+    def self.Of(exp_str)
+      location = caller_locations(1).first
+      unless File.exist? location.absolute_path
+        raise NotImplementedError, 'not now'
+      end
+
+      # caller line number
+      lineno = location.lineno
+
+      # caller s-expression
+      context_sexp =
+        File.open(location.absolute_path, 'r') do |f|
+          Parser::CurrentRuby.parse(f.read)
+        end
+
+      # TODO: static type analysis of context_sexp
+
+      # given expression to s-expression
+      target_sexp = Parser::CurrentRuby.parse(exp_str)
+
+      # TODO: eval target_sexp in context_sexp
+    end
   end
 end
